7b670cb5fd2884dbeba331dbd7e16382a1b17041
Ruolin Zheng <zheng_ruolin@hotmail.com>
2019-09-20 14:22:14 +0800

    Make company url opens up in new page

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
       {data: 'url',
       render: function(data, type, row, meta) {
         if (type == 'display') {
-          data = '<a href="' + data + '">' + data + '</a>';
+          data = '<a href="' + data + '" target="_blank">' + data + '</a>';
         }
         return data;
       }
